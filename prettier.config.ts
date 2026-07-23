52345f645df3f175893191601671435840eb698a
Dom Porada <dom@dom.engineering>
2026-07-23 19:13:38 +0200

    Update dependencies

--- prettier.config.ts
+++ prettier.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@standard-config/prettier';
 import * as pluginYAML from './src/index.ts';
 
 export default defineConfig({
+	/* oxlint-disable-next-line typescript/no-deprecated */
 	pluginOverrides: {
 		'prettier-plugin-yaml': pluginYAML,
 	},
